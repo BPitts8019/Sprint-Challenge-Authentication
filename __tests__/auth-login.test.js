919b401ac8e9314b497e00ef98c5b69d263d678a
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-27 19:10:32 -0700

    Rewrote test suite; added support for dotenv in Jest

--- __tests__/auth-login.test.js
+++ __tests__/auth-login.test.js
@@ -1,64 +1,62 @@
-const server = require("../api/server");
-const superTest = require("supertest");
-const db = require("../database/dbConfig");
-const user_db = require("../auth/auth-model");
+// const server = require("../api/server");
+// const superTest = require("supertest");
+// const db = require("../database/dbConfig");
 
-beforeEach(async () => {
-   await db("users").truncate();
-   
-   await user_db.add({
-      username: "Booris",
-      password: "pass1234"
-   });
-});
+// beforeEach(async () => {
+//    await db("users").truncate();
+//    await db("users").insert({
+//       username: "Booris",
+//       password: "pass1234"
+//    });
+// });
 
-describe("Test /api/auth/login", () => {
-   const INVALID = "Invalid Username or password";
+// describe("Test /api/auth/login", () => {
+//    const INVALID = "Invalid Username or password";
 
-   test("Empty Object", async () => {
-      const response = await superTest(server)
-         .post("/api/auth/Login")
-         .send({});
+//    test("Empty Object", async () => {
+//       const response = await superTest(server)
+//          .post("/api/auth/Login")
+//          .send({});
 
-      expect(response.status).toBe(400);
-      expect(response.type).toBe("application/json");
-      expect(response.body.message).toBe(INVALID);
-   });
-   test("No Username", async () => {
-      const response = await superTest(server)
-         .post("/api/auth/login")
-         .send({
-            password: "pass1234"
-         });
+//       expect(response.status).toBe(400);
+//       expect(response.type).toBe("application/json");
+//       expect(response.body.message).toBe(INVALID);
+//    });
+//    test("No Username", async () => {
+//       const response = await superTest(server)
+//          .post("/api/auth/login")
+//          .send({
+//             password: "pass1234"
+//          });
 
-      expect(response.status).toBe(400);
-      expect(response.type).toBe("application/json");
-      expect(response.body.message).toBe(INVALID);
-   });
-   test("No Password", async () => {
-      const response = await superTest(server)
-         .post("/api/auth/login")
-         .send({
-            username: "Booris"
-         });
+//       expect(response.status).toBe(400);
+//       expect(response.type).toBe("application/json");
+//       expect(response.body.message).toBe(INVALID);
+//    });
+//    test("No Password", async () => {
+//       const response = await superTest(server)
+//          .post("/api/auth/login")
+//          .send({
+//             username: "Booris"
+//          });
 
-      expect(response.status).toBe(400);
-      expect(response.type).toBe("application/json");
-      expect(response.body.message).toBe(INVALID);
-   });
-   test("Good Data", async () => {
-      const response = await superTest(server)
-         .post("/api/auth/login")
-         .send({
-            username: "Booris",
-            password: "pass1234"
-         });
+//       expect(response.status).toBe(400);
+//       expect(response.type).toBe("application/json");
+//       expect(response.body.message).toBe(INVALID);
+//    });
+//    test("Good Data", async () => {
+//       const response = await superTest(server)
+//          .post("/api/auth/login")
+//          .send({
+//             username: "Booris",
+//             password: "pass1234"
+//          });
 
-      expect(response.status).toBe(200);
-      expect(response.type).toBe("application/json");
-      expect(response.body).toMatch({
-         token: expect.stringContaining("$2a$14$"),
-         message: "Welcome back Booris!"
-      });
-   });
-});
+//       expect(response.status).toBe(200);
+//       expect(response.type).toBe("application/json");
+//       expect(response.body).toMatch({
+//          token: expect.stringContaining("$2a$14$"),
+//          message: "Welcome back Booris!"
+//       });
+//    });
+// });
---
+++ __tests__/auth-router.test.js
@@ -0,0 +1,82 @@
+const server = require("../api/server");
+const superTest = require("supertest");
+const db = require("../database/dbConfig");
+const users_db = require("../auth/auth-model");
+
+afterEach(async () => {
+   await db("users").truncate();
+});
+
+describe("Test /api/auth/register", () => {
+   test("No Username", async () => {
+      const response = await superTest(server)
+         .post("/api/auth/register")
+         .send({
+            password: "pass1234"
+         });
+
+      expect(response.status).toBe(400);
+      expect(response.type).toBe("application/json");
+      expect(response.body.message).toBe("Please provide a username and password.");
+   });
+
+   test("Good Data", async () => {
+      const response = await superTest(server)
+         .post("/api/auth/register")
+         .send({
+            username: "Booris",
+            password: "pass1234"
+         });
+
+      expect(response.status).toBe(201);
+      expect(response.type).toBe("application/json");
+      expect(response.body).toEqual({
+         id: 1,
+         username: "Booris"
+      });
+   });
+});
+
+describe("Test /api/auth/login", () => {
+   const INVALID = "Invalid Username or password";
+
+   test("Bad Password", async () => {
+      const rtnUser = await users_db.add({
+         username: "Booris",
+         password: "pass1234"
+      });
+      console.log(`Created User: ${JSON.stringify(rtnUser)}`);
+
+      const response = await superTest(server)
+         .post("/api/auth/login")
+         .send({
+            username: "Booris",
+            password: "bad#password"
+         });
+
+      expect(response.status).toBe(403);
+      expect(response.type).toBe("application/json");
+      expect(response.body.message).toBe(INVALID);
+   });
+   test("Good Data", async () => {
+      const rtnUser = await users_db.add({
+         username: "Booris",
+         password: "pass1234"
+      });
+      console.log(`Created User: ${JSON.stringify(rtnUser)}`);
+
+      const response = await superTest(server)
+         .post("/api/auth/login")
+         .send({
+            username: "Booris",
+            password: "pass1234"
+         });
+
+      expect(response.status).toBe(200);
+      expect(response.type).toBe("application/json");
+      // expect(response.body).toMatch({
+      //    token: expect.stringContaining("$2a$14$"),
+      //    message: "Welcome back Booris!"
+      // });
+   });
+});
