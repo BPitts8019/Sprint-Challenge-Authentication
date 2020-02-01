81de8bb4bb7ba281eb90c8edc882ea3f3b0b216e
BPitts8019 <bradleyj.pitts@gmail.com>
2020-02-01 15:42:38 -0700

    Passing all tests -- MVP Complete

--- __tests__/all-in-one.test.js
+++ __tests__/all-in-one.test.js
@@ -3,7 +3,6 @@ const superTest = require("supertest");
 const jwt = require("jsonwebtoken");
 const db = require("../database/dbConfig");
 
-
 const status = {
    OK: 200,
    CREATED: 201,
@@ -29,10 +28,17 @@ const login_user = (userData) => {
       .send(userData);
 };
 const get_jokes = (token) => {
+   if (!token) {
+      return superTest(server)
+         .get("/api/jokes");
+   }
+
    return superTest(server)
       .get("/api/jokes")
       .set("authorization", token);
 };
+
+
 
 beforeAll(async () => {
    await db("users").truncate();
@@ -76,7 +82,7 @@ describe("Test /api/auth/register", () => {
 });
 
 describe("Test /api/auth/login", () => {
-   test("Status Code 400 when no username is provided", async () => {
+   test("Returns status code 400 when no username is provided", async () => {
       const response = await login_user({
          password: "No Username"
       });
@@ -85,7 +91,7 @@ describe("Test /api/auth/login", () => {
       expect(response.body.message).toBe(GIVE_NAME_PWD);
    });
 
-   test("Status Code 400 when no password is provided", async () => {
+   test("Returns status code 400 when no password is provided", async () => {
       const response = await login_user({
          username: "No Password"
       });
@@ -94,7 +100,7 @@ describe("Test /api/auth/login", () => {
       expect(response.body.message).toBe(GIVE_NAME_PWD);
    });
 
-   test("Status Code 401 with a bad password", async () => {
+   test("Returns status code 401 when password is wrong", async () => {
       const login_res = await login_user({
          ...TEST_USER,
          password: "bad#password"
@@ -104,7 +110,7 @@ describe("Test /api/auth/login", () => {
       expect(login_res.body.message).toBe("Invalid Username or password");
    });
 
-   test("Status Code 200 and a valid token with good credentials", async () => {
+   test("Returns status Code 200 and a valid token when credentials are good", async () => {
       const login_res = await login_user(TEST_USER);
       expect(login_res.status).toBe(status.OK);
       expect(login_res.type).toBe(APP_JSON);
@@ -127,41 +133,34 @@ describe("Test /api/auth/login", () => {
    });
 });
 
-// describe("Test /api/jokes", () => {
-//    const AUTH_HEADER = "";
-//    const APP_JSON = "application/json";
-//    const MSG_NOT_PASS = "shall not pass!";
+describe("Test /api/jokes", () => {
+   const MSG_NOT_PASS = "shall not pass!";
 
-//    beforeAll(async (args) => {
-//       console.log(args);
-//       await register_user(TEST_USER);
-//    });
+   test("Returns status code 401 when no auth token is provided", async () => {
+         const res = await get_jokes();
+         expect(res.status).toBe(status.UNAUTHENTICATED);
+         expect(res.type).toBe(APP_JSON);
+         expect(res.body.you).toBe(MSG_NOT_PASS);
+   });
 
-//    test("No Authentication", async () => {
-//          const res = await get_jokes();
-//          expect(res.status).toBe(status.UNAUTHENTICATED);
-//          expect(res.type).toBe(APP_JSON);
-//          expect(res.body.you).toBe(MSG_NOT_PASS);
-//    });
+   test("Returns status code 401 when auth token is invalid", async () => {
+      const BAD_TOKEN = "this-is-a-bad-token";
 
-//    test("Not Authenticated - Bad Token", async () => {
-//       const BAD_TOKEN = "this-is-a-bad-token";
+      const res = await get_jokes(BAD_TOKEN);
+      expect(res.status).toBe(status.UNAUTHENTICATED);
+      expect(res.type).toBe(APP_JSON);
+      expect(res.body.you).toBe(MSG_NOT_PASS);
+   });
 
-//       const res = await get_jokes(BAD_TOKEN);
-//       expect(res.status).toBe(status.UNAUTHENTICATED);
-//       expect(res.type).toBe(APP_JSON);
-//       expect(res.body.you).toBe(MSG_NOT_PASS);
-//    });
+   test("Returns status code 200 when auth token is valid", async () => {
+      //login with good credentials
+      const login_res = await login_user(TEST_USER);
+      expect(login_res.status).toBe(status.OK);
 
-//    test("Authenticated", async () => {
-//       //login with good credentials
-//       const login_res = login_user(TEST_USER);
-//       expect(login_res.status).toBe(status.OK);
-
-//       //test jokes endpoint
-//       const jokes_res = await get_jokes(login_res.body.token)
-//       expect(jokes_res.status).toBe(status.OK);
-//       expect(jokes_res.type).toBe(APP_JSON);
-//       expect(jokes_res.body.length).toBeGreaterThanOrEqual(0);
-//    });
-// });
+      //test jokes endpoint
+      const jokes_res = await get_jokes(login_res.body.token)
+      expect(jokes_res.status).toBe(status.OK);
+      expect(jokes_res.type).toBe(APP_JSON);
+      expect(jokes_res.body.length).toBeGreaterThanOrEqual(0);
+   });
+});
